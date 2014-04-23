551d80fa76bf1c0b985a73626e4d9e1c7d8e10e1
Thomas Flores <thomas.flores84@eagles.ewu.edu>
2014-04-23 12:57:49 -0700

    Final Draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 // My Scripts
-$(document).foundation();
+$(document).foundation({
+  accordion: {
+    // allow multiple accordion panels to be active at the same time
+    multi_expand: true,
+  }
+});
+
+// or directly on the modal
+$('#myModal').foundation('reveal', 'open');
